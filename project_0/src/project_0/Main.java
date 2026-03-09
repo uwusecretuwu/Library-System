8b657f1d67bc6a766152a4a227645291b1190318
Angelica <angelicabaula2@gmail.cm>
2026-03-09 14:05:57 +0800

    check commit

--- project_0/src/project_0/Main.java
+++ project_0/src/project_0/Main.java
@@ -3,6 +3,8 @@ package project_0;
 public class Main {
 
 	public static void main(String[] args) {
+
+		//hello check commit angelica
 	MWindow window = new MWindow();
 	//Splashscreen Splash = new Splashscreen();
 	window.setVisible(true);
