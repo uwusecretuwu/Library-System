d5f80549a324fd868add8573b684a9c1a3b6518f
breyo <breyoxd@gmail.com>
2026-03-11 21:50:17 +0800

    Update History.java

--- project_0/src/project_0/History.java
+++ project_0/src/project_0/History.java
@@ -1,5 +1,50 @@
-package project_0;
-
-public class History {
-
+package history;
+import javax.swing.*;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.FlowLayout;
+class History extends JPanel {
+    // JPanel history = new JPanel();
+    String Title;
+    History() {
+        this.setSize(100,100);
+        this.setBackground(Color.magenta);
+    }
+    public void addpanel() {
+        
+        
+    }
+    //bb
+    //ab
+    //rb
+    //bb
+    public void addhistory(String book, String author, String borrower, String contact_number ) {
+        String title = "Borrowed Books";
+        JLabel sub_panel_title = new JLabel();
+        sub_panel_title.setText(title);
+        JPanel panel = new JPanel();
+        JLabel label = new JLabel("Borrowed Books");
+        label.setForeground(Color.red);
+        
+    }
+    //ab
+    public void addhistory(String book, String author, int quantity) {
+        String title = "Available Books";
+        JLabel sub_panel_title = new JLabel();
+        JPanel panel = new JPanel();
+        JLabel label = new JLabel("Available Books");
+        label.setForeground(Color.red);
+    
+    }
+    public void addhistory(String book, String author, String contact_number) {
+        String title = "Requested Books";
+        JLabel sub_panel_title = new JLabel();
+        JPanel panel = new JPanel();
+        JLabel label = new JLabel("Requested Books");
+        label.setForeground(Color.red);
+        
+    }
+    
 }
