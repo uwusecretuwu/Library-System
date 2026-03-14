28d2bceac8d9ced3b63f7900d215c176c7119c0c
Admin <Admin@DESKTOP-2D84R6H>
2026-03-14 14:29:22 +0800

    added colors

--- project_0/src/project_0/Colors.java
+++ project_0/src/project_0/Colors.java
@@ -3,10 +3,16 @@ package project_0;
 import java.awt.Color;
 
 public class Colors{
+	Color dark_walnut = new Color(88, 47, 14);
+	Color saddle_brown = new Color(127, 79, 36);
+	Color tofee_brown = new Color(147, 102, 57);
 	Color coffee_bean = new Color(127, 85, 57);
 	Color camel = new Color(166, 138, 100);
+	Color khaki_beige = new Color(182, 173, 144);
+	Color dry_dage = new Color(164, 172, 134);
 	Color almond_cream = new Color(237, 224, 212);
 	Color dusty_olive = new Color(101, 109, 74);
 	Color ebony = new Color(65, 72, 51);
+	Color charcoal_brown = new Color(51, 61, 41);//
 	Color black = new Color(0,0,0);
 }
